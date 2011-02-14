529664dc91ee9b1bd72ae053f77ff384f0993131
Armin Vieweg <armin@v.ieweg.de>
2011-02-14 12:16:08 +0000

    Add new viewhelper: removeWhitespaces

---
+++ Classes/ViewHelpers/RemoveWhitespacesViewHelper.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Armin Ruediger Vieweg <info@professorweb.de>
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * This view helper removes whitespaces which are annoying the HTML output,
+ * cause some browsers are interpreting tabs and new lines.
+ *
+ * @author     Armin Rüdiger Vieweg <info@professorweb.de>
+ * @copyright  2011 Copyright belongs to the respective authors
+ * @license    http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class Tx_PwTeaser_ViewHelpers_RemoveWhitespacesViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+	/**
+	 * Returns the content without dispensable whitespaces
+	 *
+	 * @return string Rendered string, may be empty.
+	 */
+	public function render() {
+		return str_replace(array("\t", "\r", "\n"), '', $this->renderChildren());
+	}
+}
+?>
