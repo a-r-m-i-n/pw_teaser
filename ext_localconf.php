1fe1bd139acc736825efb6c03bcf8ecaedabe84d
Armin Vieweg <armin@v.ieweg.de>
2011-03-22 22:52:03 +0000

    Make caching of whole plugin configurable (via extension settings)

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,12 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+$extConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+$actionNotToCache = '';
+if ($extConfiguration['ENABLECACHE'] == '0') {
+	$actionNotToCache = 'index';
+}
+
 Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY,
 	'Pi1',
@@ -10,7 +16,7 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 		'Teaser' => 'index',
 	),
 	array(
-		'Teaser' => 'index',
+		'Teaser' => $actionNotToCache,
 	)
 );
 ?>
