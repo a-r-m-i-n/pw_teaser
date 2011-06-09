7b3f71817bf39559fc58f40251fb1fed9e2f0d78
Armin Vieweg <armin@v.ieweg.de>
2011-06-09 10:31:56 +0000

    Release Version 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "pw_teaser".
 #
-# Auto generated 21-12-2011 12:35
+# Auto generated 09-06-2011 12:31
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.9.0',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-0.0.0',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:27:{s:12:"ext_icon.gif";s:4:"c590";s:17:"ext_localconf.php";s:4:"1dc0";s:14:"ext_tables.php";s:4:"1e42";s:14:"ext_tables.sql";s:4:"7e10";s:39:"Classes/Controller/TeaserController.php";s:4:"e2b5";s:32:"Classes/Domain/Model/Content.php";s:4:"0061";s:29:"Classes/Domain/Model/Page.php";s:4:"ff2f";s:31:"Classes/Domain/Model/Teaser.php";s:4:"a126";s:47:"Classes/Domain/Repository/ContentRepository.php";s:4:"876f";s:44:"Classes/Domain/Repository/PageRepository.php";s:4:"ca67";s:46:"Classes/Domain/Repository/TeaserRepository.php";s:4:"3d87";s:29:"Classes/Utilities/oelibdb.php";s:4:"b972";s:44:"Classes/ViewHelpers/GetContentViewHelper.php";s:4:"44ec";s:43:"Classes/ViewHelpers/StripTagsViewHelper.php";s:4:"f46b";s:43:"Configuration/FlexForms/flexform_teaser.xml";s:4:"7206";s:28:"Configuration/TCA/Teaser.php";s:4:"14c3";s:34:"Configuration/TypoScript/setup.txt";s:4:"76f9";s:40:"Resources/Private/Language/locallang.xml";s:4:"f699";s:77:"Resources/Private/Language/locallang_csh_tx_extteaser_domain_model_teaser.xml";s:4:"6557";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"dd67";s:49:"Resources/Private/Language/locallang_flexform.xml";s:4:"5bf2";s:38:"Resources/Private/Layouts/default.html";s:4:"4d58";s:42:"Resources/Private/Partials/formErrors.html";s:4:"f5bc";s:45:"Resources/Private/Templates/Teaser/Index.html";s:4:"478a";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:59:"Resources/Public/Icons/tx_extteaser_domain_model_teaser.gif";s:4:"905a";s:14:"doc/manual.sxw";s:4:"d379";}',
+	'_md5_values_when_last_written' => 'a:28:{s:21:"ext_conf_template.txt";s:4:"59e5";s:12:"ext_icon.gif";s:4:"c590";s:17:"ext_localconf.php";s:4:"d56d";s:14:"ext_tables.php";s:4:"14fe";s:14:"ext_tables.sql";s:4:"d41d";s:39:"Classes/Controller/TeaserController.php";s:4:"d7a1";s:32:"Classes/Domain/Model/Content.php";s:4:"fabc";s:29:"Classes/Domain/Model/Page.php";s:4:"5a7a";s:47:"Classes/Domain/Repository/ContentRepository.php";s:4:"64f9";s:44:"Classes/Domain/Repository/PageRepository.php";s:4:"ce5d";s:28:"Classes/Utility/Settings.php";s:4:"c246";s:27:"Classes/Utility/oelibdb.php";s:4:"0798";s:44:"Classes/ViewHelpers/GetContentViewHelper.php";s:4:"a30a";s:51:"Classes/ViewHelpers/RemoveWhitespacesViewHelper.php";s:4:"340c";s:43:"Classes/ViewHelpers/StripTagsViewHelper.php";s:4:"befb";s:49:"Classes/ViewHelpers/Widget/PaginateViewHelper.php";s:4:"235e";s:60:"Classes/ViewHelpers/Widget/Controller/PaginateController.php";s:4:"2ca6";s:43:"Configuration/FlexForms/flexform_teaser.xml";s:4:"7fdd";s:34:"Configuration/TypoScript/setup.txt";s:4:"43a0";s:40:"Resources/Private/Language/locallang.xml";s:4:"a32e";s:49:"Resources/Private/Language/locallang_flexform.xml";s:4:"49ce";s:38:"Resources/Private/Layouts/default.html";s:4:"4d58";s:42:"Resources/Private/Partials/formErrors.html";s:4:"f5bc";s:49:"Resources/Private/Templates/HeadlineAndImage.html";s:4:"0314";s:46:"Resources/Private/Templates/HeadlinesOnly.html";s:4:"a842";s:45:"Resources/Private/Templates/Teaser/Index.html";s:4:"b7f3";s:66:"Resources/Private/Templates/ViewHelpers/Widget/Paginate/Index.html";s:4:"0e9a";s:14:"doc/manual.sxw";s:4:"1042";}',
 );
 
 ?>
