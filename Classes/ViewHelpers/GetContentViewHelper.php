e094166ba4bf99fe1be8a42fe15979fd8ca5fdc5
Armin Vieweg <armin@v.ieweg.de>
2011-06-09 10:30:57 +0000

    Small fixes, add two example templates

--- Classes/ViewHelpers/GetContentViewHelper.php
+++ Classes/ViewHelpers/GetContentViewHelper.php
@@ -34,20 +34,16 @@
 class Tx_PwTeaser_ViewHelpers_GetContentViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
 
 	/**
+	 * Get content
 	 *
-	 * @param array<Tx_PwTeaser_Domain_Model_Content> $contents array which
-	 *        contains content elements
+	 * @param array $contents array which contains content elements
 	 * @param string $as the name of the iteration variable
-	 * @param integer $colPos column position to get content elements from,
-	 *        default is 0 (normal)
-	 * @param string $cType the cType to filter content elements for, default is
-	 *        NULL
-	 * @param integer $index limits the output to n-th element. default is NULL
-	 *        which disables the limitation, 0 would limit the output to the
-	 *        first found content element
-	 * @return string Rendered string
+	 * @param integer $colPos column position to get content elements from, default is 0 (normal)
+	 * @param string $cType the cType to filter content elements for, default is NULL
+	 * @param integer $index limits the output to n-th element. default is NULL which disables the limitation, 0 would
+	 *        limit the output to the first found content element
 	 *
-	 * @author Armin Rüdiger Vieweg <info@professorweb.de>
+	 * @return string Rendered string
 	 */
 	public function render($contents, $as, $colPos = 0, $cType = NULL, $index = NULL) {
 		if ($contents === NULL) {
