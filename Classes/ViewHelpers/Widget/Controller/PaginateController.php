33db96b0bb2b6d804ec2bd9f536c24dfbb99e982
Armin Vieweg <armin@v.ieweg.de>
2015-08-18 00:57:43 +0200

    [BUGFIX] Replace deprecated array_merge_recursive_overrule() method
    of \TYPO3\CMS\Core\Utility\GeneralUtility
    with \TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule()

--- Classes/ViewHelpers/Widget/Controller/PaginateController.php
+++ Classes/ViewHelpers/Widget/Controller/PaginateController.php
@@ -65,10 +65,9 @@ class PaginateController extends \TYPO3\CMS\Fluid\Core\Widget\AbstractWidgetCont
 	 */
 	public function initializeAction() {
 		$this->objects = $this->widgetConfiguration['objects'];
-		$this->configuration = \TYPO3\CMS\Core\Utility\GeneralUtility::array_merge_recursive_overrule(
+		\TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule(
 			$this->configuration,
-			$this->widgetConfiguration['configuration'],
-			TRUE
+			$this->widgetConfiguration['configuration']
 		);
 	}
 
