eece8531c82c2779687036c60258d99426c10fa1
Armin Vieweg <armin@v.ieweg.de>
2016-08-22 23:00:40 +0200

    [FEATURE] Update icon to png and add entry in new content element wizard

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Page Teaser (with Fluid)',
-    'description' => 'Create powerful, dynamic page teasers with data from page properties and its content elements. ' .
+    'description' => 'Create powerful, dynamic page teasers with data from page properties and its content elements.' .
         'Based on Extbase and Fluid Template Engine.',
     'category' => 'plugin',
     'shy' => 0,
