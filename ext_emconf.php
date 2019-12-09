e5385c06d966a0f3fb93210cf6cd3c3aa7198f5d
Armin Vieweg <armin@v.ieweg.de>
2019-12-09 19:57:02 +0100

    [TASK] Set version to 4.0.0-dev and update package name to "t3/pw_teaser"
    Also set TYPO3 dependency to ^8.7|^9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,11 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "pw_teaser".
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Page Teaser (with Fluid)',
     'description' => 'Create powerful, dynamic page teasers with data from page properties and its content elements. Based on Extbase and Fluid Template Engine.',
     'category' => 'plugin',
     'shy' => 0,
-    'version' => '3.4.2',
+    'version' => '4.0.0-dev',
     'dependencies' => '',
     'conflicts' => '',
     'priority' => '',
@@ -25,17 +17,17 @@ $EM_CONF[$_EXTKEY] = array(
     'modify_tables' => '',
     'clearcacheonload' => 0,
     'lockType' => '',
-    'author' => 'Armin Ruediger Vieweg',
+    'author' => 'Armin Vieweg',
     'author_email' => 'armin@v.ieweg.de',
     'author_company' => '',
     'CGLcompliance' => '',
     'CGLcompliance_note' => '',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '6.2.0-8.99.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-    'suggests' => array()
-);
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'suggests' => []
+];
