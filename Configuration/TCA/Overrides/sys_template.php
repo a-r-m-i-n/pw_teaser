3283820c1f47b6486b4a5ec9dd1a1914c341c87b
Armin Vieweg <armin@v.ieweg.de>
2020-08-04 10:38:21 +0200

    [TASK] Move ext_tables.php contents to TCA/Overrides

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'pw_teaser',
+    'Configuration/TypoScript',
+    'PwTeaser'
+);
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,4 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'PwTeaserTeam.' . 'pw_teaser',
@@ -17,10 +14,4 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignat
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $pluginSignature,
     'FILE:EXT:' . 'pw_teaser' . '/Configuration/FlexForms/flexform_teaser.xml'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'pw_teaser',
-    'Configuration/TypoScript',
-    'PwTeaser'
 );
