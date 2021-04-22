618f6be1c41876a29b0f9cc45e05527dc9ce3c9a
Armin Vieweg <armin@v.ieweg.de>
2021-04-22 11:59:32 +0200

    [BUGFIX] Fix wrong Extbase mapping (for TYPO3 10+)
    "fieldName" has been written wrong.

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -8,26 +8,26 @@ return [
     PwTeaserTeam\PwTeaser\Domain\Model\Page::class => [
         'tableName' => 'pages',
         'properties' => [
-            'navTitle' => ['fieldname' => 'nav_title'],
-            'authorEmail' => ['fieldname' => 'author_email'],
-            'tstamp' => ['fieldname' => 'tstamp'],
-            'creationDate' => ['fieldname' => 'crdate'],
-            'lastUpdated' => ['fieldname' => 'lastUpdated'],
-            'starttime' => ['fieldname' => 'starttime'],
-            'endtime' => ['fieldname' => 'endtime'],
-            'newUntil' => ['fieldname' => 'newUntil'],
-            'sorting' => ['fieldname' => 'sorting'],
-            'l18nConfiguration' => ['fieldname' => 'l18n_cfg'],
+            'navTitle' => ['fieldName' => 'nav_title'],
+            'authorEmail' => ['fieldName' => 'author_email'],
+            'tstamp' => ['fieldName' => 'tstamp'],
+            'creationDate' => ['fieldName' => 'crdate'],
+            'lastUpdated' => ['fieldName' => 'lastUpdated'],
+            'starttime' => ['fieldName' => 'starttime'],
+            'endtime' => ['fieldName' => 'endtime'],
+            'newUntil' => ['fieldName' => 'newUntil'],
+            'sorting' => ['fieldName' => 'sorting'],
+            'l18nConfiguration' => ['fieldName' => 'l18n_cfg'],
         ]
     ],
     \PwTeaserTeam\PwTeaser\Domain\Model\Content::class => [
         'tableName' => 'tt_content',
         'properties' => [
-            'pid' => ['fieldname' => 'pid'],
-            'colPos' => ['fieldname' => 'colPos'],
-            'ctype' => ['fieldname' => 'CType'],
-            'tstamp' => ['fieldname' => 'tstamp'],
-            'crdate' => ['fieldname' => 'crdate'],
+            'pid' => ['fieldName' => 'pid'],
+            'colPos' => ['fieldName' => 'colPos'],
+            'ctype' => ['fieldName' => 'CType'],
+            'tstamp' => ['fieldName' => 'tstamp'],
+            'crdate' => ['fieldName' => 'crdate'],
         ]
     ]
 ];
