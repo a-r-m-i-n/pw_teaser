c86cc18805199f44660f6eb7d9d91290cc9aad37
Armin Vieweg <armin@v.ieweg.de>
2022-09-05 16:22:45 +0200

    [TASK] Raise TYPO3 requirements
    Added v11 support
    Dropped v9 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Page Teaser (with Fluid)',
     'description' => 'Create powerful, dynamic page teasers with data from page properties and its content elements. Based on Extbase and Fluid Template Engine.',
     'category' => 'plugin',
-    'version' => '5.0.1',
+    'version' => '6.0.0-dev',
     'state' => 'stable',
     'author' => 'Armin Vieweg',
     'author_email' => 'info@v.ieweg.de',
     'author_company' => 'v.ieweg Webentwicklung',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.6-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
